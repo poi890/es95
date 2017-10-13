e53b1308b97f0e6b634463d64b8eac7b408abfa2
humzatahir <12738673+humzatahir@users.noreply.github.com>
2017-10-12 23:53:06 -0400

    updated files with working algorithm and output

--- test.py
+++ test.py
@@ -1,79 +1,118 @@
 import praw
-
-reddit = praw.Reddit(client_id='jAdMtveNKrDi9g',
-                     client_secret='6LFxnKmAM-M_fFb6aE83-FZLvQU',
-                     user_agent='python:com.example.commentcounter:v1.0.0')
-
-submissions = reddit.subreddit('EthTrader').hot(limit=10) #make top instead of hot
-
-totalComments = 0
-totalScore = 0
-print(submissions.next())
-for submission in submissions:
-    print(submission.title + ': ' + str(len(submission.comments)) + ': ' + str(submission.score)) #add sentiment of each article title too
-
-    totalComments += len(submission.comments)
-    totalScore += submission.score
-
-print('Total Comments: ', totalComments)
-print('Total Score: ', totalScore)
-
-#calculate daily score (sum of daily_sentiment + daily_total_comments + daily_score for each article within the past 24 hours)
-
-
-#other things to do: program should skip posts that have below x amount of comments (since they're most likely irrelevant)
-#posts by ethereum staff should be weighted higher
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
 
 
 
+def initReddit():
+    # init API
+    print "starting up reddit api"
+    reddit = praw.Reddit(client_id='jAdMtveNKrDi9g',
+                         client_secret='6LFxnKmAM-M_fFb6aE83-FZLvQU',
+                         user_agent='python:com.example.commentcounter:v1.0.0')
+    print "reddit API initialized"
+    return reddit
+
+def getSubmissions(unix_date, reddit):
+    # pull submissions
+    submissions = reddit.subreddit('EthTrader').submissions(start = unix_date, 
+                                                            end = unix_date + 86400) #make top instead of hot
+    # set total comment/score counters
+    print "retrieved"
+    return submissions
 
 
-
-
-
-
-
-
-
-#extra information
-#print('Number of top level comments: ', len(submission.comments))
-#for top_level_comment in submission.comments:
-#	print('Comment: ', top_level_comment.body)
-
-
-#edit below if data is to be exported to an excel file
-#with open('comments.csv', 'a') as outf:
-# 2 dimension array needs two for loops
-#for ele in final:
-#   for ele2 in ele:
-#       # write to file in the proper comma delimited format, one per line
-#       outf.write(u"{0},{1}\n".format(ele2['id'], ele2['comment']).encode('utf-8'))
+# Super Score Calculator
+def superScore(title, comment_ct, score, sentiment_dict):
+    multiplier = (1.0 - (5 * sentiment_dict['neg']) + (5 * sentiment_dict['pos']))
+    super_score = (comment_ct + score) * multiplier
+    
+    def printScore():
+        print '*******    string: ', submission.title
+        print '*******  comments: ', len(submission.comments)
+        print '*******     score: ', submission.score
+        print "*********** negative: ", sentiment_dict['neg']
+        print "***********  neutral: ", sentiment_dict['neu']
+        print "*********** positive: ", sentiment_dict['pos']
+        print "multiplier: ", multiplier
+        print "SUPERSCORE: ", super_score
+        print '\n'
+   
+    return super_score
 
 #Sentiment Analysis
-# import nltk
-#import numpy
-#from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-
-#text_file = open("wordsList.txt", "r")
-#lines = text_file.read()
-
-#sid = SIA()
-#print(sid.polarity_scores(lines))
+def sentiment(string):
+    sid = SIA()
+    return sid.polarity_scores(string)
 
 
 
 
-#Abdur's Twitter Project
-#import Twython
-# keys when registering app with twitter
-#APP_KEY = 'KqIkw34tQJHOtZS3bbYyc2EYr'
-#APP_SECRET = 'l3t3mydsl7QLWoNfm3ZdIBfjmPb1tMWbxlfe6FS1xGSSTFhfR7'
+
+def score(submission_obj): 
+    for submission in submissions:
+        #totalComments = 0
+        #totalScore = 0
+        #totalComments += len(submission.comments)
+        #totalScore += submission.score
+        print submission.title
+        superScore(submission, sentiment(submission.title))
+
+print "start"
+
+counter = 0
+reddit = initReddit()
+date = 1493596800
+submissions = getSubmissions(date, reddit) # start may 1st
 
 
-# new twython instance for authorization
-#twitter = Twython(APP_KEY, APP_SECRET)
-#    auth = twitter.get_authentication_tokens(callback_url = 'http://ide50-abdurrehman1.cs50.io/verify')
 
-# get request tokens from auth
-#OAUTH_TOKEN = auth['oauth_token']
-#    OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+#score(submissions)     
+
+#print submissions
+
+titles = []
+scores = []
+comments = []
+super_scores = []
+dates = []
+
+
+for i in range(0, 150):
+    
+    date = 1493596800 + (86400 * i)
+
+    print 'date:', datetime.datetime.utcfromtimestamp(date)
+    #retrieve data
+    reddit = initReddit()
+    submissions = getSubmissions(date, reddit)
+    
+    # create arrays to be added to pandas
+    print 'scraping and scoring...'
+    for submission in submissions:
+        title = submission.title
+        score = submission.score
+        len_comments = len(submission.comments)
+        titles.append(title)
+        scores.append(score)
+        comments.append(len_comments)
+        super_scores.append(superScore(title, len_comments, score, sentiment(title)))
+        dates.append(datetime.datetime.utcfromtimestamp(date))
+    
+
+
+df = pd.DataFrame({
+                    #'date': pd.Timestamp('20130102'), 
+                    'title': titles,
+                    'score': scores,
+                    'comments': comments,
+                    'date': dates,
+                    'super_scores' : super_scores
+                  })
+
+
+df
